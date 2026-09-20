03ebfa163e5ef4307c1e2d8e91fe1a30882f61e6
Ismael <156593686+IsmaelHS@users.noreply.github.com>
2026-09-20 22:32:32 +0100

    Add logic for the user to change the size of the grid

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// Create 16x16 divs.
-const divs = [];
+// Create a 16x16 grid.
+let divs = [];
+let divWidth = 60;
 for (let i = 0; i <= 255; i++) {
   divs.push(document.createElement("div"));
 }
 
 // Append the divs to the container div.
-const container = document.querySelector(".container");
+let container = document.querySelector(".container");
 for (let div of divs) {
+  div.setAttribute("style", `width : ${divWidth}px; height : ${divWidth}px`);
   container.appendChild(div);
 }
 
@@ -16,4 +18,35 @@ container.addEventListener("mouseover", (event) => {
 });
 container.addEventListener("mouseout", (event) => {
   event.target.classList.remove("hover");
+});
+
+// Add ability to resize
+const resize = document.querySelector(".resize");
+resize.addEventListener("click", () => {
+  try {
+    // Get the new size from the user and check if it's in proper range.
+    let size = Number(
+      prompt("Enter the number of squares per side for the new grid"),
+    );
+    if (size < 2 || size > 100 || Number.isNaN(size)) {
+      throw new Error("The size should be a number in range 2 to 100.");
+    }
+
+    // Reset the grid with the new size.
+    divs = [];
+    divWidth = Math.ceil(960 / size);
+    container.replaceChildren();
+    for (let i = 0; i < size * size; i++) {
+      divs.push(document.createElement("div"));
+    }
+    for (let div of divs) {
+      div.setAttribute(
+        "style",
+        `width : ${divWidth}px; height : ${divWidth}px`,
+      );
+      container.appendChild(div);
+    }
+  } catch (error) {
+    alert(error);
+  }
 });
